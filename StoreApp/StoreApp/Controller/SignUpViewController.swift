6e59fda61db7eb6579a7c89450c25a61052f2f6f
hoonv <chaeefe@gmail.com>
2020-10-05 17:53:33 +0900

    feat: rename viewcontroller to signupViewcontroller
    - 이름 변경

---
+++ StoreApp/StoreApp/Controller/SignUpViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  StoreApp
+//
+//  Created by 채훈기 on 2020/09/28.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var idTextView: VerifyTextView!
+    @IBOutlet weak var firstPWTextView: SecureVerifyTextView!
+    @IBOutlet weak var secondPWTextView: SecureVerifyTextView!
+    @IBOutlet weak var nameTextView: VerifyTextView!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(named: "BackgroundColor")
+        setupTextField()
+        nextButton.layer.cornerRadius = 5
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: .UIKeyboardWillShow, object: nil)
+
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    private func setupTextField() {
+        idTextView.dataSource = IdDataSource()
+        idTextView.delegate = IdDelegate()
+        firstPWTextView.dataSource = PWDataSource()
+        firstPWTextView.delegate = PWDelegate()
+        secondPWTextView.dataSource = PW1DataSource()
+        secondPWTextView.delegate = self
+        nameTextView.delegate = NameDelegate()
+        nameTextView.dataSource = NameDataSource()
+    }
+
+    @objc func keyboardWillShow(_ sender: Notification) {
+        self.view.frame.origin.y = -100
+    }
+    
+    @objc func keyboardWillHide(_ sender: Notification) {
+        self.view.frame.origin.y = 0
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    @IBAction func nextOnTouch(_ sender: Any) {
+        if idTextView.isvalid() && firstPWTextView.isvalid()
+            && secondPWTextView.isvalid() && nameTextView.isvalid(){
+            print("move")
+        }
+    }
+}
