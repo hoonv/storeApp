b5d793834ae5465db0eba678fc6da2cdb30622c3
hoonv <chaeefe@gmail.com>
2020-10-06 22:16:17 +0900

    feat: create collection view header
    - header 생성

---
+++ StoreApp/StoreApp/View/CollectionHeaderView.swift
@@ -0,0 +1,19 @@
+//
+//  CollectionHeaderView.swift
+//  StoreApp
+//
+//  Created by 채훈기 on 2020/10/06.
+//
+
+import UIKit
+
+class CollectionHeaderView: UICollectionReusableView {
+
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var desc: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
