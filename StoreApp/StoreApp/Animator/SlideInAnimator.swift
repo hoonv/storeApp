25c546fc5602ae39d506b598d3ea22814337bae4
hoonv <chaeefe@gmail.com>
2020-10-06 22:16:40 +0900

    feat: slide animation
    오른쪽으로 움직이는 애니메이션

---
+++ StoreApp/StoreApp/Animator/SlideInAnimator.swift
@@ -0,0 +1,44 @@
+//
+//  SlideInAnimator.swift
+//  StoreApp
+//
+//  Created by 채훈기 on 2020/10/06.
+//
+
+import UIKit
+
+final class SlideInAnimator: NSObject, UIViewControllerAnimatedTransitioning {
+    
+    func transitionDuration(using ctx: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0.5
+    }
+    
+    func animateTransition(using ctx: UIViewControllerContextTransitioning) {
+        guard let fromVC = ctx.viewController(forKey: .from),
+              let toVC = ctx.viewController(forKey: .to),
+              let snapshot = fromVC.view.snapshotView(afterScreenUpdates: true)
+        else {
+            return
+        }
+        let containerView = ctx.containerView
+        let initialFrame = ctx.initialFrame(for: fromVC)
+        let finalFrame = ctx.finalFrame(for: toVC)
+        snapshot.frame = initialFrame
+        containerView.addSubview(toVC.view)
+        containerView.addSubview(snapshot)
+        toVC.view.frame = finalFrame
+        toVC.view.frame.origin.x = finalFrame.width
+        UIView.animate(
+            withDuration: transitionDuration(using: ctx), animations: {
+                snapshot.frame.origin.x = -finalFrame.width
+                toVC.view.frame = finalFrame
+        }) { _ in
+            snapshot.removeFromSuperview()
+            ctx.completeTransition(!ctx.transitionWasCancelled)
+        }
+    }
+    
+    func animationEnded(_ transitionCompleted: Bool) {
+    }
+    
+}
