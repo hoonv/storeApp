be77f1af0892839e87c18b0a63f667cee8b20fab
hoonv <chaeefe@gmail.com>
2020-10-07 19:44:08 +0900

    style: change cell design
    - 셀 스타일 변경

--- StoreApp/StoreApp/View/ItemCollectionViewCell.swift
+++ StoreApp/StoreApp/View/ItemCollectionViewCell.swift
@@ -16,10 +16,15 @@ class ItemCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var sPriceLeadingConstraint: NSLayoutConstraint!
     @IBOutlet weak var nPrice: UILabel!
     @IBOutlet weak var sPrice: UILabel!
+    @IBOutlet weak var badgeStack: UIStackView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        imageView.layer.cornerRadius = imageView.frame.width / 4
+        imageView.layer.cornerRadius = imageView.frame.width / 16
         imageView.layer.masksToBounds = true
+        badgeStack.distribution  = UIStackView.Distribution.equalSpacing
+        badgeStack.alignment = UIStackView.Alignment.center
+        badgeStack.spacing   = 5
 
     }
 
