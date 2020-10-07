dbdf63183e6244439de1ce7edb8b5ef4b4acb2d0
hoonv <chaeefe@gmail.com>
2020-10-07 19:45:58 +0900

    feat: collection view cell design
    - 컬렉션뷰 셀 디자인

--- StoreApp/StoreApp/Controller/ItemListViewController.swift
+++ StoreApp/StoreApp/Controller/ItemListViewController.swift
@@ -10,19 +10,23 @@ import UIKit
 class ItemListViewController: UIViewController {
 
     @IBOutlet weak var itemCollectionView: UICollectionView!
-    let cellReuseIdentifier = "ItemCollectionViewCell"
-    let headerReuseIdentifier = "CollectionHeaderView"
-    var items: [[StoreItem]] = []
+    private let cellReuseIdentifier = "ItemCollectionViewCell"
+    private let headerReuseIdentifier = "CollectionHeaderView"
+    private let badgeColors = [#colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1), #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1), #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)]
+    private var itemViewModel = StoreItemViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        items.append(StoreItemLoader.load(name: "main") ?? [])
-        items.append(StoreItemLoader.load(name: "side") ?? [])
-        items.append(StoreItemLoader.load(name: "soup") ?? [])
+        setupCollectionView()
+    }
+    
+    private func setupCollectionView() {
         itemCollectionView.register(UINib(nibName: "ItemCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: cellReuseIdentifier)
         itemCollectionView.register(UINib(nibName: "CollectionHeaderView",bundle: nil), forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: headerReuseIdentifier)
         itemCollectionView.delegate = self
         itemCollectionView.dataSource = self
+        let layout = itemCollectionView.collectionViewLayout as? UICollectionViewFlowLayout
+        layout?.sectionHeadersPinToVisibleBounds = true
     }
 }
 
@@ -52,18 +56,22 @@ extension ItemListViewController: UICollectionViewDelegateFlowLayout {
 
 extension ItemListViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return items.count
+        return itemViewModel.items.count
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return items[section].count
+        return itemViewModel.items[section].count
     }
     
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        
         if kind.isEqual(UICollectionView.elementKindSectionHeader) {
             guard let cell = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerReuseIdentifier, for: indexPath) as? CollectionHeaderView
             else { return UICollectionReusableView() }
-            cell.backgroundColor = .systemGray2
+            cell.category.text = itemViewModel.headers[indexPath.section].category
+            cell.title.text = itemViewModel.headers[indexPath.section].title
+            cell.backgroundColor = .white
+            
             return cell
         } else {
             return UICollectionReusableView()
@@ -71,27 +79,52 @@ extension ItemListViewController: UICollectionViewDelegate, UICollectionViewData
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellReuseIdentifier, for: indexPath as IndexPath) as? ItemCollectionViewCell else { return UICollectionViewCell() }
-        let item = items[indexPath.section][indexPath.item]
-        let url = URL(string: item.image)
-        DispatchQueue.main.async {
+        let item = itemViewModel.items[indexPath.section][indexPath.item]
+        
+        // init cell
+        cell.sPriceLeadingConstraint.constant = 5
+        cell.badgeStack.arrangedSubviews.forEach {
+            $0.removeFromSuperview()
+        }
+        
+        // setup
+        cell.title.text = item.title
+        cell.desc.text = item.description
+        cell.sPrice.text = item.sPrice
+        
+        if let nPrice = item.nPrice {
+            cell.nPrice.attributedText = "\(nPrice)원".cancelLine()
+        } else {
+            cell.sPriceLeadingConstraint.constant = 0
+            cell.nPrice.text = ""
+        }
+        
+        DispatchQueue.global().async {
             do {
-                let data = try Data(contentsOf: url!)
-                cell.imageView.image = UIImage(data: data)
+                guard let url = URL(string: item.image) else { return }
+                let data = try Data(contentsOf: url)
+                DispatchQueue.main.async {
+                    cell.imageView.image = UIImage(data: data)
+                }
             } catch {
                 print("error")
             }
         }
-        cell.title.text = item.title
-        cell.desc.text = item.description
-        if let nPrice = item.nPrice {
-            cell.sPrice.text = item.sPrice
-            cell.nPrice.text = nPrice
-        } else {
-            cell.sPrice.text = item.sPrice
-            cell.sPriceLeadingConstraint.constant = 0
-            cell.nPrice.text = ""
+        
+        item.badge?.forEach {
+            let label = BadgeLabel(frame: CGRect.zero)
+            let idx = itemViewModel.badges.firstIndex(of: $0) ?? 0 % badgeColors.count
+            label.text = $0
+            label.backgroundColor = badgeColors[idx]
+            label.translatesAutoresizingMaskIntoConstraints = false
+            cell.badgeStack.addArrangedSubview(label)
+            NSLayoutConstraint.activate([
+                label.heightAnchor.constraint(equalToConstant: cell.badgeStack.frame.height),
+            ])
         }
+
         return cell
     }
 }
