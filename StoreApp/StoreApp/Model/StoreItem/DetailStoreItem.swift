a46168ad70b4a9d752b61784c9b212e23fa466e0
hoonv <chaeefe@gmail.com>
2020-10-12 23:50:50 +0900

    feat: create detailStoreItem
    - 디테일뷰를 위한 모델 생성

--- StoreApp/StoreApp/Model/StoreItem/DetailStoreItem.swift
+++ StoreApp/StoreApp/Model/StoreItem/DetailStoreItem.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+struct DetailStoreItem: Codable {
+    let hash: String
+    let data: DataClass
+}
+
+struct DataClass: Codable {
+    let topImage: String
+    let thumbImages: [String]
+    let productDescription, point, deliveryInfo, deliveryFee: String
+    let prices: [String]
+    let detailSection: [String]
+
+    enum CodingKeys: String, CodingKey {
+        case topImage = "top_image"
+        case thumbImages = "thumb_images"
+        case productDescription = "product_description"
+        case point
+        case deliveryInfo = "delivery_info"
+        case deliveryFee = "delivery_fee"
+        case prices
+        case detailSection = "detail_section"
+    }
+}
