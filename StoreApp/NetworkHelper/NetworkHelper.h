6c9fd73e56c27edefc9aeaafb9ff5d9cfb0964f4
hoonv <chaeefe@gmail.com>
2020-10-12 12:43:56 +0900

    feat: create Net helper
    - net helper 생성

---
+++ StoreApp/NetworkHelper/NetworkHelper.h
@@ -0,0 +1,18 @@
+//
+//  NetworkHelper.h
+//  NetworkHelper
+//
+//  Created by 채훈기 on 2020/10/12.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetworkHelper.
+FOUNDATION_EXPORT double NetworkHelperVersionNumber;
+
+//! Project version string for NetworkHelper.
+FOUNDATION_EXPORT const unsigned char NetworkHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkHelper/PublicHeader.h>
+
+
