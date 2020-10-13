134f95197bdcfa35fabe3d3ff4733d7e5d4ed7b0
hoonv <chaeefe@gmail.com>
2020-10-14 01:30:57 +0900

    feat: separate image setting logic to imageManager
    - 여러군대서 사용하는 함수라 따로 분리시켜줌

---
+++ StoreApp/StoreApp/Controller/ImageManager.swift
@@ -0,0 +1,46 @@
+//
+//  ImageManager.swift
+//  StoreApp
+//
+//  Created by 채훈기 on 2020/10/14.
+//
+
+import UIKit
+import NetworkHelper
+
+func setImageLocalOrNetwork(imageView: UIImageView, item: StoreItem) {
+    
+    guard
+        let fileURL = try? FileManager.default.url(for: .cachesDirectory,
+                                                   in: .userDomainMask,
+                                                   appropriateFor: nil,
+                                                   create: false)
+            .appendingPathComponent(item.detailHash) else { return }
+    
+    if FileManager.default.fileExists(atPath: fileURL.path) {
+        setImage(to: imageView, url: fileURL)
+    } else {
+        NetworkLayer.shared.downloadTaskToCache(from: item.image, name: item.detailHash) {
+            (result: Result<URL,APIError>) in
+            switch result {
+            case .success(let url):
+                setImage(to: imageView, url: url)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
+
+private func setImage(to target: UIImageView, url: URL) {
+    DispatchQueue.global().async {
+        do {
+            let data = try Data(contentsOf: url)
+            DispatchQueue.main.async {
+                target.image = UIImage(data: data)
+            }
+        } catch {
+            target.image = nil
+        }
+    }
+}
--- StoreApp/StoreApp/Controller/ItemList/ItemListViewController.swift
+++ StoreApp/StoreApp/Controller/ItemList/ItemListViewController.swift
@@ -66,42 +66,4 @@ class ItemListViewController: UIViewController {
         let layout = itemCollectionView.collectionViewLayout as? UICollectionViewFlowLayout
         layout?.sectionHeadersPinToVisibleBounds = true
     }
-    
-    private func setImageLocalOrNetwork(imageView: UIImageView, item: StoreItem) {
-        
-        guard
-            let fileURL = try? FileManager.default.url(for: .cachesDirectory,
-                                                       in: .userDomainMask,
-                                                       appropriateFor: nil,
-                                                       create: false)
-                .appendingPathComponent(item.detailHash) else { return }
-        
-        
-        if FileManager.default.fileExists(atPath: fileURL.path) {
-            setImage(to: imageView, url: fileURL)
-        } else {
-            NetworkLayer.shared.downloadTaskToCache(from: item.image, name: item.detailHash) {
-                (result: Result<URL,APIError>) in
-                switch result {
-                case .success(let url):
-                    self.setImage(to: imageView, url: url)
-                case .failure(let error):
-                    print(error)
-                }
-            }
-        }
-    }
-    
-    private func setImage(to target: UIImageView, url: URL) {
-        DispatchQueue.global().async {
-            do {
-                let data = try Data(contentsOf: url)
-                DispatchQueue.main.async {
-                    target.image = UIImage(data: data)
-                }
-            } catch {
-                target.image = nil
-            }
-        }
-    }
 }
